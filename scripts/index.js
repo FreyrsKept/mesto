473442935a2b6c4022b40c13c55c2f043f8e7700
FreyrsKept <120368406+FreyrsKept@users.noreply.github.com>
2023-03-17 00:17:57 +0700

    Fix
    js update

--- scripts/index.js
+++ scripts/index.js
@@ -33,3 +33,55 @@ function handleFormSubmit(evt) {
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
+
+const initialCards = [
+    {
+      name: 'Виноградовский мост',
+      link: 'https://top10.travel/wp-content/uploads/2017/09/vinogradovskij-most.jpg'
+    },
+    {
+      name: 'Царь рыба',
+      link: 'https://top10.travel/wp-content/uploads/2017/09/tsar-ryba.jpg'
+    },
+    {
+      name: 'Красноярские столбы',
+      link: 'https://top10.travel/wp-content/uploads/2017/09/zapovednik-stolby.jpg'
+    },
+    {
+      name: 'Вид на океан',
+      link: 'https://fikiwiki.com/uploads/posts/2022-02/1645033331_1-fikiwiki-com-p-krasivie-kartinki-na-smartfon-1.jpg'
+    },
+    {
+      name: 'Горы алтая',
+      link: 'https://www.russiadiscovery.ru/upload/files/files/Altaiskie_gory.jpg'
+    },
+    {
+      name: 'FUJIFILM C200 PHOTO',
+      link: 'https://assets.community.lomography.com/ca/d5e3b43a75575e8ec79b18eb5747ed1a96f30c/1216x789x1.jpg?auth=89bb7a413857bb6927c5e5289102859143289dd1'
+    }
+  ];
+
+  const cards = document.querySelector('.cards__template')
+
+  initialCards.forEach(function (card) {
+    const newCard = document.querySelector('#cardTemplate').content.cloneNode(true)
+    const cardHeading = newCard.querySelector('.cards__title')
+    cardHeading.textContent = card.name
+    const cardImage = newCard.querySelector('.cards__image')
+    cardImage.setAttribute('src', card.link)
+    cards.append(newCard)
+
+  })  
+
+
+//  const main = document.querySelector('.main')
+
+//   initialCards.forEach(function (card) {
+//     const newCard = document.querySelector('#cardTemplate').content.cloneNode(true)
+//     const cardHeading = newCard.querySelector('.cards__title')
+//     cardHeading.textContent = card.name
+//     const cardImage = newCard.querySelector('.cards__image')
+//     cardImage.setAttribute('src', card.link)
+//     main.append(newCard)
+
+//   })  
