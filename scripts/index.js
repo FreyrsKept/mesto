a8f7eff7b19d7dcdbc7cdb46abbd36d46ae5b0eb
FreyrsKept <120368406+FreyrsKept@users.noreply.github.com>
2023-04-21 15:19:15 +0700

    upd
    Добавлен alt для стартовых фото и для новых добавленных

--- scripts/index.js
+++ scripts/index.js
@@ -54,26 +54,32 @@ formElement.addEventListener('submit', handleFormSubmit);
 const initialCards = [
   {
     name: 'Виноградовский мост',
+    alt: 'Виноградовский мост в Красноясрке ведущий на остров-парк Татышев',
     link: 'https://top10.travel/wp-content/uploads/2017/09/vinogradovskij-most.jpg'
   },
   {
     name: 'Царь рыба',
+    alt: 'Царь рыба в Дивногорске',
     link: 'https://top10.travel/wp-content/uploads/2017/09/tsar-ryba.jpg'
   },
   {
     name: 'Красноярские столбы',
+    alt: 'Красноярские столбы - заповедник близ города Красноярск',
     link: 'https://top10.travel/wp-content/uploads/2017/09/zapovednik-stolby.jpg'
   },
   {
     name: 'Вид на океан',
+    alt: 'Вид на океан',
     link: 'https://fikiwiki.com/uploads/posts/2022-02/1645033331_1-fikiwiki-com-p-krasivie-kartinki-na-smartfon-1.jpg'
   },
   {
     name: 'Горы алтая',
+    alt: 'Вид на горы алтая',
     link: 'https://www.russiadiscovery.ru/upload/files/files/Altaiskie_gory.jpg'
   },
   {
-    name: 'FUJIFILM C200 PHOTO',
+    name: 'FUJIFILM C200 photo',
+    alt: 'Фото сделанное на FUJIFILM C200',
     link: 'https://assets.community.lomography.com/ca/d5e3b43a75575e8ec79b18eb5747ed1a96f30c/1216x789x1.jpg?auth=89bb7a413857bb6927c5e5289102859143289dd1'
   }
 ];
@@ -87,6 +93,7 @@ function createCard(card) {
   cardHeading.textContent = card.name
   const cardImage = newCard.querySelector('.cards__image')
   cardImage.setAttribute('src', card.link)
+  cardImage.setAttribute('alt', card.alt)
   cards.prepend(newCard)
 
   // Переменная для удаления карточки
@@ -96,6 +103,9 @@ function createCard(card) {
   // переменная для лайка карточки
   const likeCardButton = document.querySelector('.cards__like-button');
   likeCardButton.addEventListener('click', likeCard);
+
+  // переменная для зума картинки
+  
 }
 initialCards.forEach(createCard)
 
