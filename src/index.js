aa447e5a89af4e8e004877b471a257b9220364c7
FreyrsKept <120368406+FreyrsKept@users.noreply.github.com>
2023-08-03 21:21:03 +0700

    delete old code

--- src/index.js
+++ src/index.js
@@ -83,8 +83,7 @@ const popupImage = new PopupWithImage(viewPopup);
 
 const popupEditProfileForm = new PopupWithForm(profilePopup, (e) => {
   e.preventDefault();
-  const data = popupEditProfileForm.getInputs();
-  userInfo.setUserInfo(data);
+  userInfo.setUserInfo(userData);
   popupEditProfileForm.close();
 })
 
@@ -108,79 +107,3 @@ ProfileButtonEdit.addEventListener('click', () => {
   popupEditProfileForm.open();
   handleProfileFormSubmit(userData);
 })
-
-//////////////////old////////////////////
-// const renderCard = (card) => {
-//   cards.prepend(createCard(card));
-// };
-
-// initialCards.forEach((card) => renderCard(card));
-
-// // функция которая по сабмиту берет данные из полей, заворачивает в обьект ниже создавая карточку.
-// function handleCardFormSubmit(evt) {
-//   evt.preventDefault();
-//   renderCard({
-//     name: imageName.value,
-//     alt: imageName.value,
-//     link: imageLink.value
-//   }, cards);
-//   closePopup(popupAddCard);
-// };
-// cardAddForm.addEventListener('submit', handleCardFormSubmit);
-
-// buttonOpenPopupCard.addEventListener('click', () => {
-//   cardAddForm.reset();
-//   popupFormAddCardValidation.toggleButtonStatus();
-// })
-
-// // функция закрытия попапа
-// function closePopup(popup) {
-//   popup.classList.remove('popup_opened');
-//   document.removeEventListener('keydown', closePopupEsc);
-// };
-
-// // Закрытие попапа через 'Esc'
-// const closePopupEsc = function (evt) {
-//   if (evt.key === "Escape") {
-//     const popupOpened = document.querySelector('.popup_opened')
-//     closePopup(popupOpened);
-//   }
-// };
-
-// // закрытие попапа кликом на оверлей
-// popups.forEach( popupElement => {
-//   popupElement.addEventListener('mousedown', (evt) => {
-//     if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__close')) {
-//       closePopup(popupElement);
-//     }
-//   });
-// });
-
-// // функция открытия попапа
-// function openPopup(popup) {
-//   popup.classList.add('popup_opened')
-//   document.addEventListener('keydown', closePopupEsc);
-// };
-
-// // Открытия попапа профиля с автозаполнением полей
-// ProfileButtonEdit.addEventListener('click', function () {
-//   openPopup(profilePopup)
-//   nameInput.value = userName.textContent;
-//   jobInput.value = description.textContent;
-// });
-
-// // Сабмит изменений в профиле 
-// function handleProfileFormSubmit(evt) {
-//   evt.preventDefault();
-//   userName.textContent = nameInput.value;
-//   description.textContent = jobInput.value;
-//   closePopup(profilePopup);
-// };
-// profileChangeForm.addEventListener('submit', handleProfileFormSubmit);
-
-// // Открытие попапа добавления фото
-// buttonOpenPopupCard.addEventListener('click', function () {
-//   openPopup(popupAddCard);
-// });
-// export default viewPopup;
-// export {openPopup};
