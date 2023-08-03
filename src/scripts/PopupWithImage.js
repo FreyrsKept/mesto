898955a470e6ab53f0afc4d4da3e1d60765d7412
FreyrsKept <120368406+FreyrsKept@users.noreply.github.com>
2023-08-03 21:53:22 +0700

    fix

--- src/scripts/PopupWithImage.js
+++ src/scripts/PopupWithImage.js
@@ -3,14 +3,14 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popup) {
     super(popup);
-    this._imageTitle = this._popupCard.querySelector(".popup__caption");
-    this._imageView = this._popupCard.querySelector(".popup__image");
   }
 
   open(name, link) {
-    this._imageView.setAttribute("alt", name);
-    this._imageView.setAttribute("src", link);
-    this._imageTitle.textContent = name;
+    const imageTitle = this._popupCard.querySelector(".popup__caption");
+    const imageElement = this._popupCard.querySelector(".popup__image");
+    imageElement.alt = name;
+    imageElement.src = link;
+    imageTitle.textContent = name;
     super.open();
   }
 }
