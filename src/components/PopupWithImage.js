d8707d44da5eca27c6a7d8ea99e22eb5a8dcb8de
FreyrsKept <kazaksecondarymail@gmail.com>
2023-08-10 00:43:36 +0700

    fix

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,16 +1,16 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
-  constructor(popup) {
-    super(popup);
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._imageTitle = this._popup.querySelector(".popup__caption");
+    this._imageElement = this._popup.querySelector(".popup__image");
   }
 
   open(name, link) {
-    const imageTitle = this._popupCard.querySelector(".popup__caption");
-    const imageElement = this._popupCard.querySelector(".popup__image");
-    imageElement.alt = name;
-    imageElement.src = link;
-    imageTitle.textContent = name;
+    this._imageElement.setAttribute("alt", name);
+    this._imageElement.setAttribute("src", link);
+    this._imageTitle.textContent = name;
     super.open();
   }
 }
