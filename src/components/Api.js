203edb0dc4457f02ec0622e638416c1d98d5973d
FreyrsKept <120368406+FreyrsKept@users.noreply.github.com>
2023-08-21 21:25:16 +0700

    Added markup for avatar edit
    Start writing api

---
+++ src/components/Api.js
@@ -0,0 +1,7 @@
+export const api = new Api({
+    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-73',
+    headers: {
+      authorization: '8ffdb09c-8dca-4dba-a4c8-d13e3ae22fe9',
+      'Content-Type': 'application/json'
+    }
+  })
